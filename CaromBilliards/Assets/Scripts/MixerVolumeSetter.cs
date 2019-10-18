93b3ffa85447579f6d93aab05c4673f5e6f53bae
Dominub <thecodex@hotmail.com>
2019-10-18 15:45:09 +0200

    Task 1 complete and loading scene

---
+++ CaromBilliards/Assets/Scripts/MixerVolumeSetter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class MixerVolumeSetter : MonoBehaviour
+{    
+    public AudioMixer mixer;
+    public void SetMasterVolume(float sliderValue)
+    {        
+        mixer.SetFloat("Master", Mathf.Log10(sliderValue) * 20f);
+    }
+    public void SetSFXVolume(float sliderValue)
+    {
+        mixer.SetFloat("SFX", Mathf.Log10(sliderValue) * 20f);
+    }
+    public void SetMusicVolume(float sliderValue)
+    {
+        mixer.SetFloat("Music", Mathf.Log10(sliderValue) * 20f);
+    }
+}
